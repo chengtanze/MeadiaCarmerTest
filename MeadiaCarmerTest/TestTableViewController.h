6e366fdbf7a55a4dabe0ab08d1648b5127234e78
chengtanze <cheng@chengdeMacBook-Pro.local>
2015-02-13 22:09:19 +0800

    增加拍照，选择相片功能

---
+++ MeadiaCarmerTest/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  MeadiaCarmerTest
+//
+//  Created by cheng on 15/2/13.
+//  Copyright (c) 2015年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+- (IBAction)click:(id)sender;
+@end
